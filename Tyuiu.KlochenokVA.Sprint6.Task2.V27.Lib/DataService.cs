3b544046a0b97f28833074ca181fcd2a15d0ca08
Владимир Клоченок <Tyuiu.KlochenokV@outlook.com>
2026-04-10 20:35:53 +0500

    о

--- Tyuiu.KlochenokVA.Sprint6.Task2.V27.Lib/DataService.cs
+++ Tyuiu.KlochenokVA.Sprint6.Task2.V27.Lib/DataService.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint6;
 
 namespace Tyuiu.KlochenokVA.Sprint6.Task2.V27.Lib
 {
-    public class DataService
+    public class DataService : ISprint6Task2V27
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
---
+++ Tyuiu.KlochenokVA.Sprint6.Task2.V27.Lib/ISprint6Task2V27.cs
@@ -0,0 +1,7 @@
+namespace tyuiu.cources.programming.interfaces.Sprint6
+{
+ public interface ISprint6Task2V27
+ {
+ double[] GetMassFunction(int startValue, int stopValue);
+ }
+}
